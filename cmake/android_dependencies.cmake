477981f5160b0ed927f73b9d558f6beb2cc2d3c7
Mahdi Aslami Khavari <mahdyaslami@gmail.com>
2024-02-19 10:31:36 +0330

    Replace com.google with com.part for remote config

--- cmake/android_dependencies.cmake
+++ cmake/android_dependencies.cmake
@@ -76,7 +76,7 @@ set(FIREBASE_MESSAGING_ANDROID_DEPS
 )
 
 set(FIREBASE_REMOTE_CONFIG_ANDROID_DEPS
-    "com.google.firebase:firebase-config:21.4.1"
+    "com.part.firebase:firebase-config:21.4.1"
     "com.google.firebase:firebase-analytics:21.3.0"
 )
 
